7d91bd239664a4c8b2bb617914f4ef8790467d95
jonjo <joaogilberto@tuta.io>
2021-08-09 22:09:42 -0300

    acrescentando raise e formas de prever notas fora do escopo de 1 a 10 e uso de funções

--- quantopreciso.rb
+++ quantopreciso.rb
@@ -3,7 +3,11 @@
 # Funcoes de coleta de dados e impressao de resultado
 def peganota(description)
   puts "Digite a nota #{description} no formato X.X "
-  gets.chomp.to_f
+  x = gets.chomp.to_f
+  raise puts 'A nota não pode ser menor que zero.' if x.negative?
+  raise puts 'A nota não pode ser maior que 10.' if x > 10
+
+  x
 end
 
 def japacou
@@ -14,12 +18,27 @@ def precisade(nota)
   puts "Você precisa de no mínimo #{nota.ceil(1)} para passar."
 end
 
-# Inicio do programa
-puts 'Digite qualquer tecla para saber quanto precisa no 4º bimestre'
-puts 'Digite r para saber quanto precisa na recuperação'
-escolha = gets.chomp
+# Def do inicio do programa
+def inicio
+  puts 'Digite n para saber quanto precisa no 4º bimestre'
+  puts 'Digite r para saber quanto precisa na recuperação'
+  @escolha = gets.chomp
+  selecao
+end
 
-if escolha == 'r'
+def selecao
+  case @escolha
+  when 'r'
+    calc_media_anual
+  when 'n'
+    nota_4bim
+  else
+    puts 'Opção inválida. Escolha novamente'
+    inicio
+  end
+end
+
+def calc_media_anual
   mediaanual = peganota('da sua média anual')
   case mediaanual
   when 6..10
@@ -30,14 +49,19 @@ if escolha == 'r'
     recuperacao = 15 - (mediaanual * 2)
     precisade(recuperacao)
   end
-else
+end
+
+def nota_4bim
   bim1 = peganota('do 1º bim')
   bim2 = peganota('do 2º bim')
   bim3 = peganota('do 3º bim')
   puts "As notas digitadas foram #{bim1}, #{bim2} e #{bim3}"
-  resultado = 24 - (bim1 + bim2 + bim3)
+  @resultado = 24 - (bim1 + bim2 + bim3)
+  selecao_resultado
+end
 
-  case resultado
+def selecao_resultado
+  case @resultado
   when -6..0
     japacou
   when 10..23.9
@@ -46,3 +70,5 @@ else
     precisade(resultado)
   end
 end
+
+inicio
